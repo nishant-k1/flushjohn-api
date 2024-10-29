62c65a9fdf2fca45b5e2afd259c64b502ea199d6
Nishant <nishant.29k1@gmail.com>
2024-10-30 02:22:12 +0530

    edit blog schema

--- models/Blogs/index.js
+++ models/Blogs/index.js
@@ -20,45 +20,45 @@ const BlogsSchema = new Schema({
     required: true,
     unique: true,
   },
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  tags: {
-    type: [String],
-    default: [],
-  },
-  status: {
-    type: String,
-    enum: ["draft", "published", "archived"],
-    default: "draft",
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  comments: [
-    {
-      user: { type: Schema.Types.ObjectId, ref: "User" },
-      content: String,
-      createdAt: { type: Date, default: Date.now },
-    },
-  ],
-  coverImage: {
-    type: String,
-    default: "",
-  },
-  likes: {
-    type: Number,
-    default: 0,
-  },
-  metaDescription: {
-    type: String,
-  },
-  metaKeywords: {
-    type: [String],
-  },
+  // author: {
+  //   type: Schema.Types.ObjectId,
+  //   ref: "User",
+  //   required: true,
+  // },
+  // tags: {
+  //   type: [String],
+  //   default: [],
+  // },
+  // status: {
+  //   type: String,
+  //   enum: ["draft", "published", "archived"],
+  //   default: "draft",
+  // },
+  // category: {
+  //   type: String,
+  //   required: true,
+  // },
+  // comments: [
+  //   {
+  //     user: { type: Schema.Types.ObjectId, ref: "User" },
+  //     content: String,
+  //     createdAt: { type: Date, default: Date.now },
+  //   },
+  // ],
+  // coverImage: {
+  //   type: String,
+  //   default: "",
+  // },
+  // likes: {
+  //   type: Number,
+  //   default: 0,
+  // },
+  // metaDescription: {
+  //   type: String,
+  // },
+  // metaKeywords: {
+  //   type: [String],
+  // },
 });
 
 module.exports = mongoose.models.Blogs || mongoose.model("Blogs", BlogsSchema);
