0f9728aff97753fb5e823545f18603e995330cbd
Nishant <nishant.29k1@gmail.com>
2024-10-30 01:22:09 +0530

    sfs

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,61 +1,64 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 const Blogs = require("../models/Blogs");
 
-router.post("/", async function (req, res, next) {
+// POST: Create a new blog
+router.post("/", async function (req, res) {
   try {
     const createdAt = new Date();
-    const latestBlog = await Blogs.findOne({}, "blogNo").sort({
-      blogNo: -1,
-    });
+    const latestBlog = await Blogs.findOne({}, "blogNo").sort({ blogNo: -1 });
     const latestBlogNo = latestBlog ? latestBlog.blogNo : 999;
     const newBlogNo = latestBlogNo + 1;
-    const blogNo = newBlogNo;
-    const webblog = blogData({ ...req.body, createdAt, blogNo });
+
+    // Ensure blogData function is defined or adjust as necessary
+    const webblog = { ...req.body, createdAt, blogNo: newBlogNo };
     const blog = await Blogs.create(webblog);
-    res.io.emit("create-blog", blog);
-    res.status(201).json({ success: true, data: blog.data });
+
+    if (res.io) res.io.emit("create-blog", blog);
+    res.status(201).json({ success: true, data: blog });
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    res.status(500).json({ success: false, error: error.message });
   }
 });
 
-router.get("/", async function (req, res, next) {
+// GET: Retrieve blogs, single or all
+router.get("/", async function (req, res) {
   try {
-    // await corsHandle(req, res)
-    const { searchParams } = new URL(req.url);
-    const _id = searchParams.get("_id");
+    const _id = req.query._id;
     if (_id) {
-      const blogs = await Blogs.findById(_id);
-      res.status(200).json({ success: true, data: blogs });
+      const blog = await Blogs.findById(_id);
+      res.status(200).json({ success: true, data: blog });
     } else {
       const blogs = await Blogs.find().sort({ _id: -1 });
       res.status(200).json({ success: true, data: blogs });
     }
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    res.status(500).json({ success: false, error: error.message });
   }
 });
 
-router.put("/", async function (req, res, next) {
+// PUT: Update a blog by _id
+router.put("/", async function (req, res) {
   try {
-    const { searchParams } = new URL(req.url);
-    const _id = searchParams.get("_id");
-    const blog = await Blogs.findByIdAndUpdate(_id, req.body);
+    const _id = req.query._id;
+    const blog = await Blogs.findByIdAndUpdate(_id, req.body, { new: true });
     res.status(200).json({ success: true, data: blog });
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    res.status(500).json({ success: false, error: error.message });
   }
 });
 
-router.delete("/", async function (req, res, next) {
+// DELETE: Delete a blog by _id
+router.delete("/", async function (req, res) {
   try {
-    const { searchParams } = new URL(req.url);
-    const _id = searchParams.get("_id");
+    const _id = req.query._id;
     const blog = await Blogs.findByIdAndDelete(_id);
     res.status(200).json({ success: true, data: blog });
   } catch (error) {
-    console.log(error);
+    console.error(error);
     res.status(500).json({ success: false, error: error.message });
   }
 });
